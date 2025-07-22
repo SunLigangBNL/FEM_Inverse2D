1d4afaed94c19c83ded60752dd6c81d7af954293
sun2024 <sun2024@Ubuntu22.04.5LTS.myguest.virtualbox.org>
2025-07-22 16:09:38 -0400

    now updated to the lastest forward codes.

--- forward_model/modules/FEMProcessing.py
+++ forward_model/modules/FEMProcessing.py
@@ -5,16 +5,19 @@ This file contains the functions transforming the FEM solution in the k space to
 import os
 import numpy as np
 
-from .InterfaceTransform import kvectrans1, kvectrans2, kvectrans3, FresnelFun
+from .InterfaceTransform import karraytrans, FresnelFun
 
 # function1: transform FEM solution to intensity in Q space.
 # input: thetaarray, JCM Output folder, decayswitch, sample detector distance (unit: m).
 # output: Qxmat, Qzmat, Intensitymat. (unit: per angstrom.)
-def IntensityFEM(config, directory, decayswitch):
+# What is exactly contained in Intensitymat? I_{t,m}/I_i=Tm^2|km,z|/(E0^2*k_{i,z}). See the note at page D6.3.
+
+def IntensityFEM(config, directory):
     Qxmat=[]
     Qzmat=[]
     Intensitymat=[]
-    thetaarray=config.thetaarray
+    Psiradmat=[]
+    thetaarray=config.source.thetaarray
     for i in range(len(thetaarray)):
         theta=thetaarray[i]
         thetarad=np.radians(theta)
@@ -23,7 +26,7 @@ def IntensityFEM(config, directory, decayswitch):
         foldername = os.path.join(directory, f"theta_{theta:.2f}")
         pathkx=os.path.join(foldername, 'kxarray.txt')
         pathky=os.path.join(foldername, 'kyarray.txt')
-        #pathkz=os.path.join(foldername, 'kzarray.txt')
+        pathkz=os.path.join(foldername, 'kzarray.txt')
         
         pathexspec=os.path.join(foldername, 'exspec.txt')
         patheyspec=os.path.join(foldername, 'eyspec.txt')
@@ -31,178 +34,87 @@ def IntensityFEM(config, directory, decayswitch):
 
         # Transform coordinates: from JCM 2D to JCM 3D:
         kxarray = np.loadtxt(pathkx,dtype=complex)
-        kzarray = np.loadtxt(pathky,dtype=complex)
+        kzarray = np.loadtxt(pathky,dtype=complex) # note the changed name.
         kxarray=np.real(kxarray)
         kzarray=np.real(kzarray)
         
         exspec = np.genfromtxt(pathexspec, dtype=complex)
         eyspec = np.genfromtxt(pathezspec, dtype=complex) # note the changed name.
         ezspec = np.genfromtxt(patheyspec, dtype=complex) # note the changed name.
-    
-        # apply interface transform functions.
-        ninc=config.refindexi
-        ntrm=config.refindext
-        kivecx2, kivecz2, diffangrad2 = kvectrans1(theta, ninc, ntrm, config.k1)
-        kivecx3, kivecz3, diffangrad3 = kvectrans2(kivecx2, kivecz2, ntrm, ninc, config.k0) # note the order of the parameters!!
-        kxarray3, kzarray3, anglearray = kvectrans3(kxarray, kzarray, ntrm, ninc, config.k0) # note that we need to use the modified one!
-        FresnelR, FresnelT=FresnelFun(kxarray,kzarray,ntrm, ninc)
-    
+
+        # Release diffracted waves from substrate into free space.
+        # layerrefindexarray[-1] is always air.
+        refindexlastlayer=np.real(config.material.layerrefindexarray[-2]) # Must take the real part to coinside with the JCM setting.
+        refindexair=1.0
+        kxarray3, kzarray3, diffradarray = karraytrans(kxarrayin=kxarray, kzarrayin=kzarray, refindexin=refindexlastlayer, refindexout=refindexair,
+                                                       knormout=config.source.k0) 
+
+        # transform diffradarray to psiarray. (psiarray is the angle respecting to the transmitted kivec!!)
+        diffangrad0=-thetarad # diffraction angle of the fundamental order wave kivec (with a negative sign to the JCM [theta, phi] definition).
+        kivecx0=config.source.k0*np.sin(diffangrad0) # JCM 3D frame
+        kivecz0=-config.source.k0*np.cos(diffangrad0) # JCM 3D frame
+        psiradarray=diffradarray-diffangrad0 # psi=2theta. sample frame.
+        
+        FresnelR, FresnelT=FresnelFun(kxarrayin=kxarray,kzarrayin=kzarray,refindexin=refindexlastlayer, refindexout=refindexair)
+        
         # modify the amplitudes of the transmitted plane waves.       
         exspec2=FresnelT*exspec
         eyspec2=FresnelT*eyspec
         ezspec2=FresnelT*ezspec
         
-        # compute the far-field intensity.
-        intarray=np.real(exspec2*np.conjugate(exspec2)+eyspec2*np.conjugate(eyspec2)+ezspec2*np.conjugate(ezspec2))
+        # compute intensity array based on diffraction efficiency and conservation of energy.
+        temparray1=np.real(exspec2*np.conjugate(exspec2)+eyspec2*np.conjugate(eyspec2)+ezspec2*np.conjugate(ezspec2))
+        temparray2=np.abs(kzarray3) # here we should use free-space wave vector.
+        intarray=temparray1*temparray2/(config.source.E0**2*config.source.k0*np.abs(np.cos(thetarad)))
+
+        # coordinate transformation: from JCM 3D frame to CDSAXS sample frame.
+        kxarray4=kxarray3
+        kzarray4=-kzarray3
+        kivecx4=kivecx0
+        kivecz4=-kivecz0
         
-        # coordinate transformation: from JCM 3D to CDSAXS.
-        kqarrayx=-kxarray3
-        kqarrayz=kzarray3
-        
-        kivecqx=-kivecx3
-        kivecqz=kivecz3
-        
-        Qxarray=kqarrayx-kivecqx
-        Qzarray=kqarrayz-kivecqz
-        
-        Qxarray2=10**(-10)*Qxarray
-        Qzarray2=10**(-10)*Qzarray
+        Qxarray=kxarray4-kivecx4
+        Qzarray=kzarray4-kivecz4
         
         # sort the datasets based on increasing Qxvalue.
-        indexlist1=np.argsort(Qxarray2)
-        Qxarray3=Qxarray2[indexlist1]
-        Qzarray3=Qzarray2[indexlist1]
+        indexlist1=np.argsort(Qxarray)
+        Qxarray2=Qxarray[indexlist1]
+        Qzarray2=Qzarray[indexlist1]
+        intarray2=intarray[indexlist1]
+        psiradarray2=psiradarray[indexlist1]
         
-        intarray2=intarray[indexlist1] # now intarray2 has the sorted order.
-        anglearray2=anglearray[indexlist1] # now anglearray2 has the sorted order.
-        
-        if decayswitch==1:
-            #print("Decay factor 1/r included.")
-            distancearray=config.rr/np.cos(anglearray2)
-            decayarray=1/distancearray
-            intarray3=decayarray*intarray2
-        elif decayswitch==2:
-            #print("Decay factor 1/r^2 included.")
-            distancearray=config.rr/np.cos(anglearray2)
-            decayarray=1/distancearray**2
-            intarray3=decayarray*intarray2
-        else:
-            #print("No decay factor included.")
-            intarray3=intarray2
-        
-        Qxmat.append(Qxarray3)
-        Qzmat.append(Qzarray3)
-        Intensitymat.append(intarray3)
+        Qxmat.append(Qxarray2)
+        Qzmat.append(Qzarray2)
+        Intensitymat.append(intarray2)
+        Psiradmat.append(psiradarray2)
     
     Qxmat=np.array(Qxmat, dtype=object)
     Qzmat=np.array(Qzmat, dtype=object)
     Intensitymat=np.array(Intensitymat, dtype=object)
+    Psiradmat=np.array(Psiradmat, dtype=object)
 
-    return Qxmat, Qzmat, Intensitymat
+    return Qxmat, Qzmat, Intensitymat, Psiradmat
 
 # function2:
-# input: Qxmat, Qzmat, Intensitymat (unit per angstrom).
-# output: matmeta in regular grid (unit per angstrom).
-def GetMatmeta(config, Qxmat, Qzmat, Intensitymat):
-    thetaarray=config.thetaarray
-    periodicityx=config.pitch*10**10 # unit: angstrom.
-    deltaqx=2*np.pi/periodicityx # unit: per angstrom.
-    dimqx=2*config.dimqxbranch+1 # number of all interested Qx.
-    qxrefarray=np.linspace(-config.dimqxbranch*deltaqx,config.dimqxbranch*deltaqx,dimqx)
+# input: Qxmat, Qzmat, Intensitymat (SI unit).
+# output: matmeta in regular grid (SI unit).
+# Remark: matmeta[:,:,3] contains the original JCM thetaarray. (not the defined sample rotation angle.)
+# Remark: matmeta[:,:,4] contains delta diffraction angle 2theta.
+def GetMatmeta(config, Qxmat, Qzmat, Intensitymat, Psiradmat):
+    thetaarray=config.source.thetaarray
+    deltaqx=config.deltaqx
+    qxrefarray=np.linspace(-config.dimqxbranch*config.deltaqx,config.dimqxbranch*config.deltaqx,2*config.dimqxbranch+1)
     
-    matmeta=np.zeros((len(thetaarray),dimqx,4)) # per angle, per Qx location, store (Qx, Qz, Intensity, theta) values.
+    matmeta=np.zeros((len(thetaarray),2*config.dimqxbranch+1,5)) # per angle, per Qx location, store (Qx, Qz, Intensity, theta, psi) values.
     for i in range(len(thetaarray)):
         qxarray=np.array(Qxmat[i])
         for j,qxvalue in enumerate(qxrefarray):
             index1 = np.argmin(np.abs(qxarray-qxvalue))
-            if np.abs(qxarray[index1]-qxvalue)<0.5*deltaqx:
+            if np.abs(qxarray[index1]-qxvalue)<0.5*deltaqx: # the 0.5 here is good for both periodic grating and finite grating.
                 matmeta[i,j,0]=Qxmat[i][index1]
                 matmeta[i,j,1]=Qzmat[i][index1]
                 matmeta[i,j,2]=Intensitymat[i][index1]
                 matmeta[i,j,3]=thetaarray[i]
+                matmeta[i,j,4]=Psiradmat[i][index1]
     print("matmeta generated.")
     return matmeta
-
-# function3: transform results from dameon (instead of directory) to intensity in Q space.
-# This function will only be used inside ModelEvaluate.
-# input: thetaarray, dameon results, decayswitch.
-# output: Qxmat, Qzmat, Intensitymat. (unit: per angstrom.)
-def IntensityFEM2(config, results, decayswitch):
-    Qxmat=[]
-    Qzmat=[]
-    Intensitymat=[]
-    thetaarray=config.thetaarray
-    for i in range(len(thetaarray)):
-        theta=thetaarray[i]
-        
-        result=results[i]
-        farresult=result[3] # corresponding to the "fourier_modes_minus_y.jcm" in the project.jcmp file.
-        karray=farresult["K"]
-        earray=farresult["ElectricFieldStrength"][0]
-
-        kxarray=np.real(karray[:,0])
-        kzarray=np.real(karray[:,1]) # note the changed name.
-    
-        exspec=earray[:,0]
-        ezspec=earray[:,1] # note the changed name.
-        eyspec=earray[:,2] # note the changed name.
-    
-        # apply interface transform functions.
-        ninc=config.refindexi
-        ntrm=config.refindext
-        kivecx2, kivecz2, diffangrad2 = kvectrans1(theta, ninc, ntrm, config.k1)
-        kivecx3, kivecz3, diffangrad3 = kvectrans2(kivecx2, kivecz2, ntrm, ninc, config.k0) # note the order of the parameters!!
-        kxarray3, kzarray3, anglearray = kvectrans3(kxarray, kzarray, ntrm, ninc, config.k0) # note that we need to use the modified one!
-        FresnelR, FresnelT=FresnelFun(kxarray,kzarray,ntrm, ninc)
-    
-        # modify the amplitudes of the transmitted plane waves.      
-        exspec2=FresnelT*exspec
-        eyspec2=FresnelT*eyspec
-        ezspec2=FresnelT*ezspec
-        
-        # compute the far-field intensity.
-        intarray=np.real(exspec2*np.conjugate(exspec2)+eyspec2*np.conjugate(eyspec2)+ezspec2*np.conjugate(ezspec2))
-        
-        # coordinate transformation: from JCM 3D to CDSAXS.
-        kqarrayx=-kxarray3
-        kqarrayz=kzarray3
-        
-        kivecqx=-kivecx3
-        kivecqz=kivecz3
-        
-        Qxarray=kqarrayx-kivecqx
-        Qzarray=kqarrayz-kivecqz
-        
-        Qxarray2=10**(-10)*Qxarray
-        Qzarray2=10**(-10)*Qzarray
-        
-        # sort the datasets based on increasing Qxvalue.
-        indexlist1=np.argsort(Qxarray2)
-        Qxarray3=Qxarray2[indexlist1]
-        Qzarray3=Qzarray2[indexlist1]
-        
-        intarray2=intarray[indexlist1] # now intarray2 has the sorted order.
-        anglearray2=anglearray[indexlist1] # now anglearray2 has the sorted order.
-        if decayswitch==1:
-            #print("Decay factor 1/r included.")
-            distancearray=config.rr/np.cos(anglearray2)
-            decayarray=1/distancearray
-            intarray3=decayarray*intarray2
-        elif decayswitch==2:
-            #print("Decay factor 1/r^2 included.")
-            distancearray=config.rr/np.cos(anglearray2)
-            decayarray=1/distancearray**2
-            intarray3=decayarray*intarray2
-        else:
-            #print("No decay factor included.")
-            intarray3=intarray2
-        
-        Qxmat.append(Qxarray3)
-        Qzmat.append(Qzarray3)
-        Intensitymat.append(intarray3)
-    
-    Qxmat=np.array(Qxmat, dtype=object)
-    Qzmat=np.array(Qzmat, dtype=object)
-    Intensitymat=np.array(Intensitymat, dtype=object)
-
-    return Qxmat, Qzmat, Intensitymat
--- forward_model/modules/InterfaceTransform.py
+++ forward_model/modules/InterfaceTransform.py
@@ -1,112 +1,53 @@
 """
-This file contains all the functions about:
-(1) wave vector transforms in a layered medium.
+This file contains the following functions:
+(1) wave vector transform from final substrate layer into free space.
 (2) modification of amplidudes based on transmission/reflection coefficients (Fresnel's equations).
 """
 
 import numpy as np
 
-# function 1: process of the single fundamental order wave vector from free space to substrate medium.
-# input: theta in JCM thetaphi.
-# output: kxvalue, kzvalue under JCM 3D coordinate system.
-# output2: diffracted angle under our own definition in radian. there is a negative sign compared with JCM thetaphi definition!
-def kvectrans1(theta, nin, nout, knormout):
-    thetain = np.radians(-theta)
-    sinthetaout=nin*np.sin(thetain)/nout
-    angradout=np.arcsin(sinthetaout)
-    kxout=knormout*np.sin(angradout)
-    kzout=-knormout*np.cos(angradout)
-    return kxout, kzout, angradout
+"""
+function 1: Transform wave vectors from substrate medium to free space.
+Inputs: (JCM 3D frame)
+    kxarrayin, kzarrayin: wave vectors under JCM 3D coordinate system.
+    refindexin: refractive index of input medium.
+    refindexout: refractive index of output medium (usually 1).
+    knormout: |k| in output medium = 2π/λ * n_out.
+Returns:
+    kxarrayout, kzarrayout (JCM 3D frame). 
+    angradout: diffraction angles in radian. (With a negative sign to the JCM [theta, phi] definition.)
+"""
+def karraytrans(kxarrayin, kzarrayin, refindexin, refindexout, knormout):
+    if (np.real(refindexin) > 1.0) or (np.abs(np.imag(refindexin)) > 0):
+        print(f"ERROR: refective index of the substratge = {refindexin} is >1 or has an imaginary part. Aborted.")
+        sys.exit(1)
+        
+    thetain = np.arctan2(kxarrayin, -kzarrayin) # from JCM angle to our defined angle frame.
+    sinthetaout = refindexin * np.sin(thetain) / refindexout
+    angradarrayout=np.arcsin(sinthetaout)
+    kxarrayout=knormout*np.sin(angradarrayout)
+    kzarrayout=-knormout*np.cos(angradarrayout)
+    return kxarrayout, kzarrayout, angradarrayout
 
-# function 2: process of the single fundamental order wave vector from substrate medium to free space.
-# input: kxin, kzin under JCM 3D coordinate system.
-# output: kxout, kzout under JCM 3D coordinate system.
-def kvectrans2(kxin, kzin, nin, nout, knormout):
-    thetain=np.atan(-kxin/kzin) # curcial point.
-    sinthetaout=nin*np.sin(thetain)/nout
-    if abs(sinthetaout)>1: # this case will only triger when nin is larger than nout.
-        print("Special Case!!")
-        angradout=100
-        kxout=0
-        kzout=0
-    else:
-        angradout=np.arcsin(sinthetaout)
-        kxout=knormout*np.sin(angradout)
-        kzout=-knormout*np.cos(angradout)    
-    return kxout, kzout, angradout
-
-# function 3: bulk transforms of all diffracted wave vectors from substrate medium to free space.
-# input: kxin, kzin under JCM 3D coordinate system.
-# output: kxout, kzout under JCM 3D coordinate system.
-def kvectrans3(kxin, kzin, nin, nout, knormout):
-    thetain=np.atan(-kxin/kzin) # curcial point.
-    sinthetaout=nin*np.sin(thetain)/nout
-    if np.max(abs(sinthetaout))<=1:
-        indexlist= [i for i, tempvalue in enumerate(sinthetaout) if abs(tempvalue)>1]
-        angradout=[]
-        kxout=[]
-        kzout=[]
-        for i in range(len(kxin)):
-            if i in indexlist:
-                kxout.append(0)
-                kzout.append(0)
-                angradout.append(100)
-            else:
-                kxvalue=kxin[i]
-                kzvalue=kzin[i]
-                thetainvalue=np.atan(-kxvalue/kzvalue)
-                sinthetaoutvalue=nin*np.sin(thetainvalue)/nout
-                angradoutvalue=np.arcsin(sinthetaoutvalue)
-                kxoutvalue=knormout*np.sin(angradoutvalue)
-                kzoutvalue=-knormout*np.cos(angradoutvalue)
-                kxout.append(kxoutvalue)
-                kzout.append(kzoutvalue)
-                angradout.append(angradoutvalue)
-        kxout=np.array(kxout)
-        kzout=np.array(kzout)
-        angradout=np.array(angradout)
-    else:
-        print("Special Case!!")
-        angradout=np.arcsin(sinthetaout)
-        kxout=knormout*np.sin(angradout)
-        kzout=-knormout*np.cos(angradout)
-    return kxout, kzout, angradout
-
-# function 4.
-# input: kxin, kzin (arrays in the substrate), and nin, nout.
-# output: refcoeff and transcoeff arrays (to adjust the amplitudes of the total electric fields in the free space).
-# Remark: only valid for P-polarization which is the realistic case.
-def FresnelFun(kxin, kzin, nin, nout):
-    thetain=np.atan(-kxin/kzin) # curcial point.
-    sinthetaout=nin*np.sin(thetain)/nout
-    if np.max(abs(sinthetaout))<=1:
-        indexlist= [i for i, tempvalue in enumerate(sinthetaout) if abs(tempvalue)>1]
-        FresnelarrayR=[]
-        FresnelarrayT=[]
-        for i in range(len(kxin)):
-            if i in indexlist:
-                FresnelarrayR.append(0)
-                FresnelarrayT.append(0)
-            else:
-                kxvalue=kxin[i]
-                kzvalue=kzin[i]
-                thetainvalue=np.atan(-kxvalue/kzvalue) # useful angle 1.
-                sinthetaoutvalue=nin*np.sin(thetainvalue)/nout
-                angradoutvalue=np.arcsin(sinthetaoutvalue) # useful angle 2.
-
-                FresnelvalueR=(nout*np.cos(thetainvalue)-nin*np.cos(angradoutvalue))/(nout*np.cos(thetainvalue)+nin*np.cos(angradoutvalue))
-                FresnelarrayR.append(FresnelvalueR)
-                FresnelvalueT=2*nin*np.cos(thetainvalue)/(nout*np.cos(thetainvalue)+nin*np.cos(angradoutvalue))
-                FresnelarrayT.append(FresnelvalueT)
-
-        FresnelarrayR=np.array(FresnelarrayR)
-        FresnelarrayT=np.array(FresnelarrayT)
-    else:
-        print("Special case!!")
-        FresnelarrayR = np.zeros_like(kxin)
-        FresnelarrayT = np.ones_like(kxin)
-
+"""
+function 2: transmission coefficients.
+input: kxin, kzin (arrays in the substrate), and nin, nout.
+output: refcoeff and transcoeff arrays (to adjust the amplitudes of the total electric fields in the free space).
+Remark: only valid for P-polarization, which is the realistic case.
+"""
+def FresnelFun(kxarrayin, kzarrayin, refindexin, refindexout):
+    if (np.real(refindexin) > 1.0) or (np.abs(np.imag(refindexin)) > 0):
+        print(f"ERROR: refective index of the substratge = {refindexin} is >1 or has an imaginary part. Aborted.")
+        sys.exit(1)
+    
+    thetain = np.arctan2(kxarrayin, -kzarrayin) # from JCM angle to our defined angle frame.      
+    sinthetaout = refindexin*np.sin(thetain)/refindexout           
+    angradout = np.arcsin(sinthetaout)   # principal branch
+    cosin  = np.cos(thetain)
+    cosout = np.cos(angradout)                 
+    temp = refindexout*cosin + refindexin*cosout  
+    # P‑polarized Fresnel formulas
+    FresnelarrayR = (refindexout*cosin-refindexin*cosout)/temp
+    FresnelarrayT = 2*refindexin*cosin/temp
     return FresnelarrayR, FresnelarrayT
-
-
 
