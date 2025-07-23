909e0d01c4ec40a662325cd9c3edda374346cd92
sun2024 <sun2024@Ubuntu22.04.5LTS.myguest.virtualbox.org>
2025-07-23 16:22:41 -0400

    jcm files and keys files upgraded.

--- forward_model/modules/construct_poly.py
+++ forward_model/modules/construct_poly.py
@@ -1,15 +1,12 @@
 #! /usr/bin/env python3
-"""Constructs a series of polygons from input data.
-
+"""
+Constructs a series of polygons from input data.
 """
 import numpy as np
 
-
 def polygons(keys):
-    """Construct the oxid and material lines and the computational domain.
-
-    :param keys: 
-
+    """
+    Construct the material scatterers and the computational domain.
     """
     cd = keys["cd"]
     h = keys["h"]
@@ -21,89 +18,49 @@ def polygons(keys):
     o1 = keys["height_offset_substrate"]
     o2 = keys["height_offset_air"]
 
+    y1=(-1)*o1
+    y2=0
+    y3=h
+    y4=h+o2
+
     # computational domain
-    comp_dom = [
-        -.5 * keys["pitch"], -o1,
-        .5 * keys["pitch"], -o1,
-        .5 * keys["pitch"], o2,
-        -.5 * keys["pitch"], o2,
+    comp_domain = [
+        -0.5*keys["pitch"], y1,
+        0.5*keys["pitch"], y1,
+        0.5*keys["pitch"], y4,
+        -0.5*keys["pitch"], y4,
     ]
 
-    # oxide layer
+    # scatterer domain.
     y_ox_0 = 0
-    y_ox_h = h
+    y_ox_h = h    
+    
+    dx=0.5*h*tan(np.deg2rad(90-swa)) # always positive.
+    x1 = -0.5*keys["pitch"]
+    x2 = -0.5*cd-dx
+    x3 = -0.5*cd+dx
+    x4 = 0.5*cd-dx
+    x5 = 0.5*cd+dx
+    x6 = 0.5*keys["pitch"]
 
-    yc = 0.5 * (y_ox_h - y_ox_0)
-    hc1 = y_ox_0 - yc
-    hc2 = y_ox_h - yc
-    dx1 = hc1 * np.tan(np.deg2rad(90 - swa))
-    dx2 = hc2 * np.tan(np.deg2rad(90 - swa))
-
-    x1 = -.5 * keys["pitch"]
-    x2 = -.5 * cd - dx2
-    x3 = -.5 * cd - dx1
-    x4 = .5 * cd + dx1
-    x5 = .5 * cd + dx2
-    x6 = .5 * keys["pitch"]
-
-    line_poly = [
-        x6, -o1,
-        x6, y_ox_0,
-        x5, y_ox_0,
-        x4, y_ox_h,
-        x3, y_ox_h,
-        x2, y_ox_0,
-        x1, y_ox_0,
-        x1, -o1
+    scatter_poly = [
+    	x1, y1,
+        x6, y1,
+        x6, y2,
+        x5, y2,
+        x4, y3,
+        x3, y3,
+        x2, y2,
+        x1, y2
     ]
 
-    # # regular material layer
-    # y_0 = y_ox_0 - t
-    # y_h = y_ox_h - t
-
-    # angle_d = 0.5 * (180 - swa)
-    # dx = t / np.tan(np.deg2rad(angle_d))
-
-    # x1 = x1
-    # x2 = x2 + dx
-    # x3 = x3 + dx
-    # x4 = x4 - dx
-    # x5 = x5 - dx
-    # x6 = x6
-
-    # material_poly = [
-    #     x6, -o1,
-    #     x6, y_0,
-    #     x5, y_0,
-    #     x4, y_h,
-    #     x3, y_h,
-    #     x2, y_0,
-    #     x1, y_0,
-    #     x1, -o1
-    # ]
-
+    # substrate domain.
     substrate_poly=[
-        x6, -o1,
-        x6, y_ox_0,
-        x1, y_ox_0,
-        x1, -o1
+    	x1, y1,
+        x6, y1,
+        x6, y2,
+        x1, y2
     ]
 
-    return substrate_poly, line_poly, comp_dom
+    return comp_domain, scatter_poly, substrate_poly 
 
-
-if __name__ == "__main__":
-    keys = dict()
-    keys["cd"] = 25.3787
-    keys["h"] = 48.0829
-    keys["swa"] = 86.9811
-    keys["t"] = 4.9393
-    keys["r_top"] = 10.3689
-    keys["r_bot"] = 4.7952
-    keys["pitch"] = 50
-    keys["height_offset_substrate"] = 20
-    keys["height_offset_air"] = 70
-
-    m, o, c = polygons(keys)
-    print("mat: {}".format(m))
-    print("ox: {}".format(o))
--- forward_model/modules/keys.py
+++ forward_model/modules/keys.py
@@ -1,38 +1,38 @@
 #! /usr/bin/env python3
 
-
 # some initial values
 layout_geometry_keys = {
-    "pitch": 80, # fixed.
-    "cd": 30, # fixed.
-    "h": 30, # to be updated.
-    "swa": 71.56505117707799, # fixed. 71.56505117707799
+    "pitch": 80,
+    "cd": 30, 
+    "h": 30, 
+    "swa": 71.56505117707799,
     "r_top": 8.0,
     "r_bot": 5.0
 }
 
 layout_keys = {
-    #"msl": 1,
-    "height_offset_substrate": 10, # always positive.
-    "height_offset_air": 40,
-    "rad_upper_n": 5,
-    "rad_lower_n": 5,
-}
-
-project_keys = {
-    "precision": 1e-3,
+    "msl": 1, # maximum side length.
+    "height_offset_substrate": 1, # always positive.
+    "height_offset_air": 1, # always positive.
+    "rad_upper_n": 5, # must be integer.
+    "rad_lower_n": 5, # must be integer.
 }
 
 sources_keys = {
-    "theta": 45,
+    "theta": 0,
     "phi": 0,
+    "lambda0": 1e-9
 }
 
 materials_keys = {
-    "epsilonr_1": 0.999435,   # substrate: Si.
-    "epsilonr_2": 0.99964+0.0000295399*1j,  # line: PMMA. Note the format here!!
-    "epsilonr_3": 1,                     # Air.
+    "epsilonr_sub": 0.999435,   # epsr for substrate.
+    "epsilonr_scat": 9.99435131564694e-1+3.278431066866658e-5*1j  # epsr for scatterer. Note the Python format here!!
 }
+
+project_keys = {
+    "numerical_aperture": 1
+}
+
 
 keys = dict()
 
